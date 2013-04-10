9726296f23240ed6735ec46ee8470e86aeb4bd28
Robert Balent <rbalent@redhat.com>
2013-04-10 13:54:44 +0200

    Starting maven project with example test

---
+++ src/main/java/cz/muni/pb138/log4j/Main.java
@@ -0,0 +1,11 @@
+package cz.muni.pb138.log4j;
+
+/**
+ * Hello world!
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
