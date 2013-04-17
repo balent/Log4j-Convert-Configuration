e91e72693c0104675fd7701e733026073634495b
Robert Balent <rbalent@redhat.com>
2013-04-17 22:29:54 +0200

    Added parsing of property file

--- src/main/java/cz/muni/pb138/log4j/PropsToXmlConverter.java
+++ src/main/java/cz/muni/pb138/log4j/PropsToXmlConverter.java
@@ -1,11 +1,21 @@
 package cz.muni.pb138.log4j;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
 
 public class PropsToXmlConverter implements Converter {
 
+    private static Logger log = Logger.getLogger(PropsToXmlConverter.class);
+    
     public void convert(File sourceFile, File outputFile) throws Exception {
-        // TODO Auto-generated method stub
+        FileInputStream fis = new FileInputStream(sourceFile);
+        
+        Properties properties = new Properties();
+
+        // now you have properties object and you can work with it.
     }
 
 }
