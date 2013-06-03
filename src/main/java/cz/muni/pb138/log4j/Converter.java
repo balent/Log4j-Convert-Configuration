8db936251ce514de70593861d2904fc6265e8e41
Stanislav Komanec <komanec@kodarna.cz>
2013-06-03 16:30:56 +0200

    Added jdoc

--- src/main/java/cz/muni/pb138/log4j/Converter.java
+++ src/main/java/cz/muni/pb138/log4j/Converter.java
@@ -8,7 +8,22 @@ import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 
 public interface Converter {
+    
+    /**
+     * Method for converting files from source to output file
+     * 
+     * @param sourceFile - path to source file
+     * @param outputFile - path to destination file
+     * @throws Exception  - throws if application fails
+     */
     public void convert(File sourceFile, File outputFile) throws Exception;
     
+    /**
+     * Method for converting files from source to output file
+     * 
+     * @param inputStream - inputStream of source file
+     * @param outputStream - outputStream to destination file
+     * @throws Exception - throws if application fails
+     */
     public void convert(InputStream inputStream, OutputStream outputStream) throws Exception;
 }
