8354208e485c6ec27a00798153f27084502dd0de
Robert Balent <rbalent@redhat.com>
2013-06-04 02:29:26 +0200

    Changed packaging of application, one jar with dependencies

--- CompileAndRun.sh
+++ CompileAndRun.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 mvn clean install
-chmod a+x ./target/log4j-convert/bin/log4j-convert
-./target/log4j-convert/bin/log4j-convert -i sample.properties
+java -jar target/log4j-convert-1.0-SNAPSHOT-jar-with-dependencies.jar -i realConfs/teiid-jboss-log4j.xml
